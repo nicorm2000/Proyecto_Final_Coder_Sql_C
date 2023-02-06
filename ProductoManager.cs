e5ca3daf14918d192c84ce5d5ad9ebfb929612d3
nicorm <nicorm2000@gmail.com>
2023-02-06 16:38:23 -0300

    created product manager and added attriubutes to product

--- ProductoManager.cs
+++ ProductoManager.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 
 namespace Proyecto_Final_Coder2023
 {
-    internal class Producto
+    internal class ProductoManager
     {
-        private long id;
-        private string descripciones;
-        private decimal costo;
     }
 }

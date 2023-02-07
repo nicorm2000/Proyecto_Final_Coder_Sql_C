d250e4ab223a73aacbb6cebd2cdfb8d03c9c647d
nicorm <nicorm2000@gmail.com>
2023-02-07 15:48:17 -0300

    Updated Prodcut Sale Manager its mostly the same but the command is changed

--- Managers/ProductSaleManager.cs
+++ Managers/ProductSaleManager.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Proyecto_Final_Coder2023.Managers
 {
-    internal class ProductSaleManager
+    public class ProductSaleManager : DBConnection
     {
+        public List<Producto> GetProductSales(int id)
+        {
+            List<Producto> producto = new List<Producto>();
+
+            query = "SELECT * FROM ProductoVendido PV INNER JOIN Producto P ON PV.IdProducto = P.Id WHERE P.Id = @id";
+
+            try
+            {
+                command.CommandText = query;
+                command.Parameters.AddWithValue("@id", id);
+                conn.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Producto ProductoTemporal = new Producto();
+
+                        ProductoTemporal.Id = reader.GetInt64(4);
+                        ProductoTemporal.Descripciones = reader.GetString(5);
+                        ProductoTemporal.Costo = reader.GetDecimal(6);
+                        ProductoTemporal.PrecioVenta = reader.GetDecimal(7);
+                        ProductoTemporal.Stock = reader.GetInt32(8);
+                        ProductoTemporal.IdUsuario = reader.GetInt64(9);
+
+                        producto.Add(ProductoTemporal);
+                    }
+                }
+                return producto;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
     }
 }

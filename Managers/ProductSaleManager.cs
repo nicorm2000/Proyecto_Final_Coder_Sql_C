a107b40182ac50a30253f2b706457d2cdf4661d4
nicorm <nicorm2000@gmail.com>
2023-02-07 15:33:29 -0300

    Added every manager adn database connection file

---
+++ Managers/ProductSaleManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_Coder2023.Managers
+{
+    internal class ProductSaleManager
+    {
+    }
+}

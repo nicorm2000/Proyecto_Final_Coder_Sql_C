4ed33b81eea5aa1d796242680b353c28231819a1
nicorm <nicorm2000@gmail.com>
2023-02-08 21:33:53 -0300

    Fixed an issue with user Id

--- Managers/ProductSaleManager.cs
+++ Managers/ProductSaleManager.cs
@@ -9,16 +9,16 @@ namespace Proyecto_Final_Coder2023.Managers
 {
     public class ProductSaleManager : DBConnection
     {
-        public List<Producto> GetProductSales(int id)
+        public List<Producto> GetProductSales(long idUsuario)
         {
             List<Producto> producto = new List<Producto>();
 
-            query = "SELECT * FROM ProductoVendido PV INNER JOIN Producto P ON PV.IdProducto = P.Id WHERE P.Id = @id";
+            query = $"SELECT IdProducto FROM Venta INNER JOIN ProductoVendido ON Venta.Id = ProductoVendido.IdVenta WHERE IdUsuario = {idUsuario}";
 
             try
             {
                 command.CommandText = query;
-                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@idUsuario", idUsuario);
                 conn.Open();
 
                 SqlDataReader reader = command.ExecuteReader();
@@ -29,12 +29,12 @@ namespace Proyecto_Final_Coder2023.Managers
                     {
                         Producto ProductoTemporal = new Producto();
 
-                        ProductoTemporal.Id = reader.GetInt64(4);
-                        ProductoTemporal.Descripciones = reader.GetString(5);
-                        ProductoTemporal.Costo = reader.GetDecimal(6);
-                        ProductoTemporal.PrecioVenta = reader.GetDecimal(7);
-                        ProductoTemporal.Stock = reader.GetInt32(8);
-                        ProductoTemporal.IdUsuario = reader.GetInt64(9);
+                        ProductoTemporal.Id = reader.GetInt64(0);
+                        ProductoTemporal.Descripciones = reader.GetString(1);
+                        ProductoTemporal.Costo = reader.GetDecimal(2);
+                        ProductoTemporal.PrecioVenta = reader.GetDecimal(3);
+                        ProductoTemporal.Stock = reader.GetInt32(4);
+                        ProductoTemporal.IdUsuario = reader.GetInt64(5);
 
                         producto.Add(ProductoTemporal);
                     }
